31ee5c0a734a03011488d7f66353bf3b75a6bead
Kamthe Sujit <sujitk@thoughtworks.com>
2023-04-05 11:42:48 +0530

    fix rules

--- src/main/java/com/example/spotlessbug/SpotlessBugApplication.java
+++ src/main/java/com/example/spotlessbug/SpotlessBugApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpotlessBugApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpotlessBugApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpotlessBugApplication.class, args);
+  }
 }
--- src/main/java/com/example/spotlessbug/Test.java
+++ src/main/java/com/example/spotlessbug/Test.java
@@ -1,18 +1,17 @@
-package com.
-        example.
-        spotlessbug;
+package com.example.spotlessbug;
 
 public class Test {
 
-    void foo() {
-        String x ="asdlkasd;akas";
-        String y = """
+  void foo() {
+    String x = "asdlkasd;akas";
+    String y =
+        """
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                
+
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -20,13 +19,15 @@ public class Test {
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                 """;
 
-        String z = """
+    String z =
+        """
                 {
                     "name": "simple json"
                 }
                 """;
 
-        String zz = """
+    String zz =
+        """
                 { "name"   : "Alice Brown",
                    "sku"    : "54321",
                    "price"  : 199.95,
@@ -42,5 +43,5 @@ public class Test {
                                 "zip"   : "98999" }
                  }
                 """;
-    }
+  }
 }
